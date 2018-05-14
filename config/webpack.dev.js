73e45691d6096834eafc6c0ebbdbb5727cc39ccb
Deivid <deividsouza18@gmail.com>
2018-05-14 12:17:50 -0300

    primeiro commit

---
+++ config/webpack.dev.js
@@ -0,0 +1,73 @@
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: {
+        main: ["./src/main.js"]
+    },
+    mode: "development",
+    output: {
+        filename: "[name]-bundle.js",
+        path: path.resolve(__dirname, "../dist"),
+        publicPath: "/"
+    },
+    devServer: {
+        contentBase: "dist",
+        overlay: true,
+        hot: true,
+        stats: {
+            colors: true
+        }
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: "babel-loader"
+                    }
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.jpg$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "images/[name]-[hash:8].[ext]"
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new HTMLWebpackPlugin({
+            template: "./src/index.ejs",
+            title: "Link's Journal"
+        })
+    ]
+}
